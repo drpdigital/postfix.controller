bbbc9d0bd09f8a72b0584d139709e45da9c27867
Alexander Mills <alex.mills@drpgroup.com>
2014-05-02 09:07:08 +0100

    added alias form to edit mailbox

--- templates/edit-mailbox.php
+++ templates/edit-mailbox.php
@@ -36,8 +36,33 @@
       <td><?php print $PALANG['pCreate_mailbox_active'] . ":"; ?></td>
       <td><input class="flat" type="checkbox" name="fActive" <?php print (!empty ($tActive)) ? 'checked' : '' ?> /></td>
       <td>&nbsp;</td>
+    </tr>
+</table>
+<br>
+<table>
+   <tr>
+      <td colspan="3"><h3><?php print $PALANG['pEdit_alias_welcome']; ?></h3></td>
    </tr>
    <tr>
+      <td><?php print $PALANG['pEdit_alias_address'] . ":"; ?></td>
+      <td><?php print $fUsername; ?></td>
+      <td>&nbsp;</td>
+   </tr>
+   <tr>
+      <td><?php print $PALANG['pEdit_alias_goto'] . ":"; ?></td>
+      <td>
+<textarea class="flat" rows="10" cols="60" name="fGoto">
+<?php print $tGoto; ?>
+</textarea>
+      </td>
+      <td>&nbsp;</td>
+   </tr>
+   <tr>
+      <td colspan="3" class="standout"><?php print $tMessage; ?></td>
+   </tr>
+</table>
+<table>
+    <tr>
       <td colspan="3" class="hlp_center">
         <input class="button" type="submit" name="submit" value="<?php print $PALANG['pEdit_mailbox_button']; ?>" />
         <input class="button" type="submit" name="cancel" value="<?php print $PALANG['exit']; ?>" action="main.php" />
